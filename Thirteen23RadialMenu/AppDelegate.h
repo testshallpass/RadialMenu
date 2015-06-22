facdb625498a1a6a70b9edf051d1e6a9c218b3c0
briguy <briansinnicke@gmail.com>
2015-06-22 16:22:54 -0400

    Initial Commit

---
+++ Thirteen23RadialMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Thirteen23RadialMenu
+//
+//  Created by Brian Sinnicke on 6/22/15.
+//  Copyright (c) 2015 Brian Sinnicke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Thirteen23RadialMenu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Thirteen23RadialMenu
+//
+//  Created by Brian Sinnicke on 6/22/15.
+//  Copyright (c) 2015 Brian Sinnicke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
